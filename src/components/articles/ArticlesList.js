8b95964c62855f51d71f516f89213e8ff94f85b8
Jameka Echols <jameka.echols@gmail.com>
2019-04-22 13:45:56 -0500

    adding sessionsstorage variable

--- src/components/articles/ArticlesList.js
+++ src/components/articles/ArticlesList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 
 export default class ArticlesList extends Component {
+    userId = sessionStorage.getItem("userId");
     
     render () {
         return (
