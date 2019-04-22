0b3a0727e783153e8069d7b2a3d3b798de330de8
Jisie DM <jisie.david@gmail.com>
2019-04-22 10:02:30 -0500

    Reactive nutshell boilerplate

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,55 @@
+import { Route } from "react-router-dom";
+import React, { Component } from "react";
+
+export default class ApplicationViews extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+
+        <Route
+          exact path="/login" render={props => {
+            return null
+            // Remove null and return the component which will handle authentication
+          }}
+        />
+
+        <Route
+          exact path="/" render={props => {
+            return null
+            // Remove null and return the component which will show news articles
+          }}
+        />
+
+        <Route
+          path="/friends" render={props => {
+            return null
+            // Remove null and return the component which will show list of friends
+          }}
+        />
+
+        <Route
+          path="/messages" render={props => {
+            return null
+            // Remove null and return the component which will show the messages
+          }}
+        />
+
+        <Route
+          path="/events" render={props => {
+            return null
+            // Remove null and return the component which will show the user's events
+          }}
+        />
+
+        <Route
+          path="/tasks" render={props => {
+            return null
+            // Remove null and return the component which will show the user's tasks
+          }}
+        />
+
+      </React.Fragment>
+    );
+  }
+}
