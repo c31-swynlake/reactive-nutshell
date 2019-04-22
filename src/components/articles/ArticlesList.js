ef107b5f508a9494e1074bffe287b18d3146885d
Jameka Echols <jameka.echols@gmail.com>
2019-04-22 13:42:55 -0500

    error

---
+++ src/components/articles/ArticlesList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+
+export default class ArticlesList extends Component {
+    userId = sessionStorage.getItem("userId")
+    render () {
+        return (
+            
+        )
+    }
+}
