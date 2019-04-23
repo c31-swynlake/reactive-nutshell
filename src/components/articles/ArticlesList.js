386c64edf1b44f36c98af5d5be00d5c23749b798
Jameka Echols <jameka.echols@gmail.com>
2019-04-23 13:47:52 -0500

    added state

--- src/components/articles/ArticlesList.js
+++ src/components/articles/ArticlesList.js
@@ -5,40 +5,41 @@ import {
 } from 'reactstrap';
 import TheArticle from './TheArticle'
 import FriendsArticle from './FriendArticles'
+import ArticleManager from '../../modules/ArticleManager'
 
 
 
 
 export default class ArticlesList extends Component {
-    // userId = sessionStorage.getItem("credentials.userId")
-    // userId = 1;
-
-    // state = {
-    //     articles: [],
-    //     friends: []
-    // }
+   
+    state = {
+        userArticles: [],
+        friendsArticles: []
+    }
     
     // componentDidMount() {
     //     const theNewState = {}
 
         
     // }
-    friends = userId => {
-        let friends = this.props.friends.filter(theFriend => theFriend.currentUserId === userId)
+    // friends = userId => {
+    //     let friends = this.props.friends.filter(theFriend => theFriend.currentUserId === userId)
 
-        friends.forEach(friend => {
-            this.props.articles.filter(theArticle => theArticle.userId === friend.id)
-            .map(friendArticle =>
-                <div key={friendArticle.id}>
-                    {friendArticle.title}
-                </div>    
-            )
-        });
-    }
+    //     friends.forEach(friend => {
+    //         this.props.articles.filter(theArticle => theArticle.userId === friend.id)
+    //         .map(friendArticle =>
+    //             <div key={friendArticle.id}>
+    //                 {friendArticle.title}
+    //             </div>    
+    //         )
+    //     });
+    // }
 
     render() {
-        const userId = 1;
+        // const userId = 1;
 
+        const userId = sessionStorage.getItem("userId")
+        console.log(userId)
         return (
             <div>
                 <Card>
@@ -52,7 +53,7 @@ export default class ArticlesList extends Component {
                         }
                     </CardBody>
                     <CardBody>
-                        <FriendsArticle theFriends={friends} friends={this.props.friends} articles={this.props.articles}/>
+                        {/* <FriendsArticle theFriends={friends} friends={this.props.friends} articles={this.props.articles}/> */}
                     </CardBody>
                 </Card>
             </div>
