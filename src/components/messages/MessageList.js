0f45bd5f658914391a70d470a618c86ca6b5a8d5
Billy Mathison <wlmathison@gmail.com>
2019-04-24 10:51:26 -0500

    fixing issue 5. Adding function to post new message in Application views, passing it down to MessageList, and limiting the numnber of messages shown on DOM to 10 messages

--- src/components/messages/MessageList.js
+++ src/components/messages/MessageList.js
@@ -3,32 +3,62 @@
 
 import React, { Component } from "react"
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button, CardHeader, CardFooter, Input
+    Card, CardBody, Form,
+    Button, CardHeader, CardFooter, Input
 } from "reactstrap"
 import MessageIndividual from "./MessageIndividual"
 
 export default class MessageList extends Component {
+    state = {
+        message: ""
+    }
+
+    handleFieldChange = event => {
+        const stateToChange = {}
+        stateToChange[event.target.id] = event.target.value
+        this.setState(stateToChange)
+    }
+
+    handleSaveMessage = event => {
+        event.preventDefault()
+
+        if (this.state.message === "") {
+            window.alert("Please enter a message")
+        } else {
+            this.props.postNewMessage({
+                userId: this.props.activeUser,
+                message: this.state.message
+            })
+        }
+    }
 
     render() {
+        let n = this.props.messages.length
+
         return (
             <React.Fragment>
                 <Card>
                     <CardHeader>Chat Room</CardHeader>
                     <CardBody>
                         {
-                            this.props.messages.map(message =>
+                            this.props.messages.slice(n - 10, n).map(message =>
                                 <MessageIndividual key={message.id} message={message} {...this.props} users={this.props.users} />
                             )
                         }
                     </CardBody>
                     <CardFooter>
-                        <Input
-                            placeholder="Enter chat message" />
-                        <Button
-                            color="info">
-                            Save Message
+                        <Form>
+                            <Input
+                                id="message"
+                                placeholder="Enter chat message"
+                                onChange={this.handleFieldChange} />
+                            <Button
+                                color="info"
+                                onClick={this.handleSaveMessage}
+                            >
+                                Save Message
                         </Button>
+                        </Form>
                     </CardFooter>
                 </Card>
             </React.Fragment>
